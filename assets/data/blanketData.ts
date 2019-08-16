fcf95beca4b3821ab9445b3f7d15a71c2b0e6eab
shaoro <52579116+shaoro@users.noreply.github.com>
2019-08-16 12:46:03 -0400

    Add files via upload

---
+++ assets/data/blanketData.ts
@@ -0,0 +1,8 @@
+import { Blanket } from '../../app/blanket';
+
+export const BlanketData: Blanket[] = [
+  {style: 'Stripe', color: 'Red and White', material: 'Cotton'},
+  {style: 'Faux Fur', color: 'Grey', material: 'Fur'},
+  {style: 'Knitting', color: 'Pink', material: 'Fiber'},
+  {style: 'crochet', color: 'Black', material: 'Fiber'},
+]
